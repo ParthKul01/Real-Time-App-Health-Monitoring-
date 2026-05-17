836e065b5bf23475e6635ea07a9b9157ce5022e9
Parth <kulkarniparth48@gmail.com>
2026-05-17 13:36:22 +0530

    api chnage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://13.63.171.197:5000',
         changeOrigin: true,
       },
     },
